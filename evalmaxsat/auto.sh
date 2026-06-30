875a1a6b75a216df6e65b743bca474f3bb7d9bf0
dev2points <24020277@vnu.edu.vn>
2026-06-30 15:03:52 +0700

    run evalmaxsat POSE

--- evalmaxsat/auto.sh
+++ evalmaxsat/auto.sh
@@ -2,14 +2,14 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
-# ./POSE.sh "$@"
-# ./POSEno.sh "$@"
+./POSE.sh "$@"
+./POSEno.sh "$@"
 # ./DSE.sh "$@"
 # ./DSEno.sh "$@"
 # ./CARD_seqcounter.sh "$@"
 # ./CARD_seqcounter_no.sh "$@"
-./CARD_totalizer.sh "$@"
-./CARD_totalizer_no.sh "$@"
+# ./CARD_totalizer.sh "$@"
+# ./CARD_totalizer_no.sh "$@"
 # # ./CARD_ladder.sh "$@"
 # ./CARD_ladder_no.sh "$@"
 # ./CARD_kmtotalizer.sh "$@"
